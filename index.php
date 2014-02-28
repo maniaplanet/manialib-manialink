55574c0fb02c50d5b8bec3dffb10fbd226f1478a
gou1 <maximeraoust@gmail.com>
2014-02-28 12:15:24 +0100

    Example

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ require_once __DIR__.'/libraries/autoload.php';
 header('Content-Type: text/xml; charset=utf-8');
 
 $manialink = new Manialink();
+$manialink->appendChild(\ManiaLib\Manialink\Elements\Redirect::create()->setNodeValue('hahaha'));
 $manialink->appendChild(Timeout::create());
 $manialink->appendChild(Quad::create()->setAttribute('sizen', '20 20')->setAttribute('posnX', '3.12'));
-
 
 $renderer = new Renderer();
 $renderer->setRoot($manialink);
