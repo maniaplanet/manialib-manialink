ee9cfdba27bba45f261c6e32a3af0476d074d191
gou1 <maximeraoust@gmail.com>
2014-02-28 12:14:56 +0100

    Manialink attributes

--- libraries/ManiaLib/Manialink/Elements/Manialink.php
+++ libraries/ManiaLib/Manialink/Elements/Manialink.php
@@ -2,9 +2,40 @@
 
 namespace ManiaLib\Manialink\Elements;
 
-class Manialink extends \ManiaLib\Manialink\Node
+use ManiaLib\Manialink\Node;
+
+class Manialink extends Node
 {
 
 	const XML_TAG_NAME = 'manialink';
+
+	function __construct()
+	{
+		$this->setVersion(1);
+	}
+
+	/**
+	 * @return \static
+	 */
+	function setVersion($value)
+	{
+		return $this->setAttribute('version', $value);
+	}
+
+	/**
+	 * @return \static
+	 */
+	function setBackground($value)
+	{
+		return $this->setAttribute('background', $value);
+	}
+
+	/**
+	 * @return \static
+	 */
+	function setNavigable3d($value)
+	{
+		return $this->setAttribute('navigable3d', $value);
+	}
 
 }
