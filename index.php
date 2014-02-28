b92b67f41b0aab4ca5b0eb0eef2505c2c35c367a
gou1 <maximeraoust@gmail.com>
2014-02-28 16:50:09 +0100

    Layouts

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
+use ManiaLib\Manialink\Elements\Frame;
+use ManiaLib\Manialink\Elements\Label;
 use ManiaLib\Manialink\Elements\Manialink;
 use ManiaLib\Manialink\Elements\Quad;
 use ManiaLib\Manialink\Elements\Redirect;
+use ManiaLib\Manialink\Elements\Script;
 use ManiaLib\Manialink\Elements\Timeout;
+use ManiaLib\Manialink\Layouts\Line;
 use ManiaLib\Manialink\Renderer;
 use ManiaLib\Manialink\Styles\Bgs1;
+
+error_reporting(E_ALL);
 
 require_once __DIR__.'/libraries/autoload.php';
 
@@ -14,8 +20,20 @@ header('Content-Type: text/xml; charset=utf-8');
 $manialink = new Manialink();
 $manialink->appendChild(Redirect::create()->setNodeValue('hahaha'));
 $manialink->appendChild(Timeout::create());
-$manialink->appendChild(Quad::create()->setAttribute('sizen', '20 20')->setAttribute('posnX', '3.12')->setStyle(Bgs1::BgWindow1));
-$manialink->appendChild(\ManiaLib\Manialink\Elements\Script::create()->setNodeValue('< yes " " >'));
+
+$frame = Frame::create()->setPosn(10, 5, 1);
+$frame->appendChild(Quad::create()->setSizen(20, 20)->setPosn(10, 2, 23.3)->setStyle(Bgs1::BgWindow1));
+$frame->appendChild(Label::create()->setSizen(50, 5)->setPosn(10, -5, 23)->setText('hello world'));
+$manialink->appendChild($frame);
+
+$manialink->appendChild(Script::create()->setNodeValue('< yes " " >'));
+
+$frame = Frame::create()->setLayout(new Line());
+$frame->appendChild(Quad::create()->setSizen(20, 20));
+$frame->appendChild(Quad::create()->setSizen(20, 20));
+$frame->appendChild(Quad::create()->setSizen(20, 20));
+$frame->appendChild(Quad::create()->setSizen(20, 20));
+$manialink->appendChild($frame);
 
 $renderer = new Renderer();
 $renderer->setRoot($manialink);
---
+++ layouts.php
@@ -0,0 +1,26 @@
+<?php
+
+use ManiaLib\Manialink\Elements\Frame;
+use ManiaLib\Manialink\Elements\Manialink;
+use ManiaLib\Manialink\Elements\Quad;
+use ManiaLib\Manialink\Layouts\Line;
+use ManiaLib\Manialink\Renderer;
+
+error_reporting(E_ALL);
+
+require_once __DIR__.'/libraries/autoload.php';
+
+header('Content-Type: text/xml; charset=utf-8');
+
+$manialink = new Manialink();
+
+$frame = Frame::create()->setLayout(new Line());
+$frame->appendChild(Quad::create()->setSizen(20, 20));
+$frame->appendChild(Quad::create()->setSizen(20, 20));
+$manialink->appendChild($frame);
+
+$renderer = new Renderer();
+$renderer->setRoot($manialink);
+echo $renderer->getXML();
+
+
