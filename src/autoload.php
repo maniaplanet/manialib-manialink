1db2966e26daf6026293b70a4a232097cdcb07ec
gou1 <maximeraoust@gmail.com>
2014-03-03 13:35:34 +0100

    tmp

--- src/autoload.php
+++ src/autoload.php
@@ -26,6 +26,6 @@ spl_autoload_register(function ($className)
 
 if(file_exists(__DIR__.'/../vendor/autoload.php'))
 {
-	require_once __DIR__.'/../vendor/autoload.php';
+	//require_once __DIR__.'/../vendor/autoload.php';
 }
 	
