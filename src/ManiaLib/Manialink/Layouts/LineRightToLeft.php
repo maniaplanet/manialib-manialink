dd570eadce8684bea4a4cd9f2a842bd242b1227d
Baptiste Lafontaine <baptiste33@gmail.com>
2014-04-15 09:51:37 +0200

    Changed LineRightToLeft behavior to start from the right of the frame if it has size (+ fix behavior)

--- src/ManiaLib/Manialink/Layouts/LineRightToLeft.php
+++ src/ManiaLib/Manialink/Layouts/LineRightToLeft.php
@@ -4,15 +4,20 @@ namespace ManiaLib\Manialink\Layouts;
 
 class LineRightToLeft extends AbstractLayout
 {
-
+	protected $init = false;
+	
 	function preFilter(\ManiaLib\Manialink\Elements\Base $node)
 	{
-
+		if (!$this->init && $this->getParent())
+		{
+			$this->xIndex = $this->getParent()->getSizenX();
+			$this->init = true;
+		}
+		$this->xIndex -= $node->getRealSizenX() + $this->marginWidth;
 	}
 
 	function postFilter(\ManiaLib\Manialink\Elements\Base $node)
 	{
-		$this->xIndex -= $node->getRealSizenX() - $this->marginWidth;
 	}
 
 }
