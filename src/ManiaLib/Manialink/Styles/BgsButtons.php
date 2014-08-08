60fc45449bd32eb608634eef535b3eed9d3c2634
satanasdiabolo <philippe@nadeo.com>
2014-08-08 13:00:22 +0200

    Added BgsButtons styles

---
+++ src/ManiaLib/Manialink/Styles/BgsButtons.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ManiaLib\Manialink\Styles;
+
+abstract class BgsButtons
+{
+
+    const BgButtonLarge = 'BgsButtons:BgButtonLarge';
+    const BgButtonMedium = 'BgsButtons:BgButtonMedium';
+    const BgButtonMediumSelector = 'BgsButtons:BgButtonMediumSelector';
+    const BgButtonMediumSpecial = 'BgsButtons:BgButtonMediumSpecial';
+    const BgButtonSmall = 'BgsButtons:BgButtonSmall';
+    const BgButtonSmall2 = 'BgsButtons:BgButtonSmall2';
+    const BgButtonXSmall = 'BgsButtons:BgButtonXSmall';
+
+}
