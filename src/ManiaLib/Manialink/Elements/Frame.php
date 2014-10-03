5fb60ae9d332cc9f0bc2797ee6bada30af4b272d
Baptiste Lafontaine <baptiste@nadeo.com>
2014-10-03 11:12:50 +0200

    Added clip, clipsizen, clipposn related methods

--- src/ManiaLib/Manialink/Elements/Frame.php
+++ src/ManiaLib/Manialink/Elements/Frame.php
@@ -16,6 +16,112 @@ class Frame extends Base
      * @var AbstractLayout
      */
     protected $layout;
+    
+    /**
+     * @return static
+     */
+    function setClip($clip = 1) {
+        return $this->setAttribute('clip', $clip);
+    }
+    
+    function getClip() {
+        return $this->getAttribute('clip');
+    }
+    
+    function getClipPosn()
+    {
+        $posn = explode(' ', $this->getAttribute('clipposn', '0 0'));
+        if (count($posn) != 2) {
+            throw new Exception('ClipPosn should be a 2-element array');
+        }
+        return $posn;
+    }
+
+    function getClipPosnX()
+    {
+        $posn = $this->getClipPosn();
+        return $posn[0];
+    }
+
+    function getClipPosnY()
+    {
+        $posn = $this->getClipPosn();
+        return $posn[1];
+    }
+
+    /**
+     * @return static
+     */
+    function setClipPosn($posnX = null, $posnY = null)
+    {
+        $posnX = $posnX === null ? $this->getClipPosnX() : $posnX;
+        $posnY = $posnY === null ? $this->getClipPosnY() : $posnY;
+        return $this->setAttribute('clipposn', (float)$posnX . ' ' . (float)$posnY);
+    }
+
+    /**
+     * @return static
+     */
+    function setClipPosnX($posnX)
+    {
+        return $this->setClipPosn($posnX, null);
+    }
+
+    /**
+     * @return static
+     */
+    function setClipPosnY($posnY)
+    {
+        return $this->setClipPosn(null, $posnY);
+    }
+    
+    function getClipSizen()
+    {
+        $posn = explode(' ', $this->getAttribute('clipsizen', '0 0'));
+        if (count($posn) != 2) {
+            throw new Exception('ClipSizen should be a 2-element array');
+        }
+        return $posn;
+    }
+
+    function getClipSizenX()
+    {
+        $posn = $this->getClipSizen();
+        return $posn[0];
+    }
+
+    function getClipSizenY()
+    {
+        $posn = $this->getClipSizen();
+        return $posn[1];
+    }
+
+    /**
+     * @return static
+     */
+    function setClipSizen($posnX = null, $posnY = null)
+    {
+        $posnX = $posnX === null ? $this->getClipSizenX() : $posnX;
+        $posnY = $posnY === null ? $this->getClipSizenY() : $posnY;
+        return $this->setAttribute('clipsizen', (float)$posnX . ' ' . (float)$posnY);
+    }
+
+    /**
+     * @return static
+     */
+    function setClipSizenX($posnX)
+    {
+        return $this->setClipSizen($posnX, null);
+    }
+
+    /**
+     * @return static
+     */
+    function setClipSizenY($posnY)
+    {
+        return $this->setClipSizen(null, $posnY);
+    }
+
 
     function registerListeners(EventDispatcherInterface $dispatcher)
     {
